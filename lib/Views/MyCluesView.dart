29ac892f73a59ebc4f9c6ab3f290b61f46df2bdd
Mazlin Higbee <mhigb411@mail.endicott.edu>
2019-03-26 21:01:04 -0400

    Added nicer listview tiles

--- lib/Views/MyCluesView.dart
+++ lib/Views/MyCluesView.dart
@@ -3,8 +3,7 @@ import 'package:scoped_model/scoped_model.dart';
 import 'package:triva/Models/Clue.dart';
 import 'package:triva/Models/SettingsModel.dart';
 
-//https://stackoverflow.com/questions/50530152/how-to-create-expandable-listview-in-flutter exandable list tile..
-
+//https://proandroiddev.com/flutter-thursday-02-beautiful-list-ui-and-detail-page-a9245f5ceaf0 used for making nice looking tiles
 class MyCluesView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
@@ -17,10 +16,7 @@ class MyCluesView extends StatelessWidget {
               return ListView.builder(
                   itemCount: Clue.clues.length,
                   itemBuilder: (context, i) {
-                    return ExpansionTile(
-                      title: Text(Clue.clues[i].question),
-                      children: <Widget>[_showAnswer(Clue.clues[i])],
-                    );
+                    return makeCard(context, Clue.clues[i]);
                   });
             } else if (snapshot.hasError) {
               return Text("Error fetching data");
@@ -32,7 +28,67 @@ class MyCluesView extends StatelessWidget {
         );
   }
 
-  Widget _showAnswer(Clue clue) {
-    return Text(clue.answer, style: TextStyle(color: Colors.red));
+  Widget makeCard(BuildContext context, Clue clue) {
+    return Card(
+      elevation: 8.0,
+      margin: new EdgeInsets.symmetric(horizontal: 10.0, vertical: 6.0),
+      child: Container(
+        decoration: BoxDecoration(color: Colors.blue[800]),
+        child: makeListTile(context, clue),
+      ),
+    );
+  }
+
+  Widget makeListTile(BuildContext context, Clue clue) {
+    return ListTile(
+        onTap: () {
+          _showAnswer(context, clue);
+        },
+        contentPadding: EdgeInsets.symmetric(horizontal: 20.0, vertical: 10.0),
+        title: Text(
+          clue.question,
+          style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold),
+        ),
+        subtitle: Row(
+          children: <Widget>[
+            Icon(Icons.monetization_on, color: Colors.yellowAccent),
+            Text(" ${clue.value}", style: TextStyle(color: Colors.white))
+          ],
+        ),
+        trailing: Icon(
+          Icons.keyboard_arrow_right,
+          color: Colors.white,
+          size: 30.0,
+        ));
+  }
+
+  Future<void> _showAnswer(BuildContext context, Clue clue) async {
+    return showDialog<void>(
+      context: context,
+      barrierDismissible: false, // user must tap button!
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: Text('What is....?'),
+          content: SingleChildScrollView(
+            child: ListBody(
+              children: <Widget>[
+                Text(
+                  "${clue.answer}",
+                  style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+                ),
+              ],
+            ),
+          ),
+          actions: <Widget>[
+            FlatButton(
+              child: Text('Dismiss'),
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+            ),
+          ],
+        );
+      },
+    );
   }
 }
